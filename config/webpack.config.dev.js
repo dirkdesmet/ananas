39589fd517535453ae46b9728f3c325f317f10c0
Dirk De Smet <15632862+dirkdesmet@users.noreply.github.com>
2017-08-07 14:30:22 +0200

    Added Ananas website

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -4,38 +4,54 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	devtool: 'cheap-eval-source-map',
-	entry: [
-		'webpack-dev-server/client?http://localhost:8080',
-		'./src/index'
-	],
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new HtmlWebpackPlugin({
-			template: './src/index.html'
-		}),
-		new ExtractTextPlugin('./dist/css/style.css', {
-			publicPath: '/css/',
-			allChunks: true
-		})
-	],
-	module: {
-		rules: [{
-			test: /\.scss$/,
-			use: ExtractTextPlugin.extract({
-				fallback: 'style-loader',
-				use: 'css-loader!sass-loader'
-			})
-		}]
-	},
-	devServer: {
-		contentBase: './dist',
-		inline: true,
-		open: true,
-		openPage: ''
-	}
+    devtool: 'cheap-eval-source-map',
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:8080',
+        'webpack/hot/only-dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin('./dist/css/style.css', {
+            publicPath: '/css/',
+            allChunks: true
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    module: {
+        rules: [{
+			test: /\.jpe?g$|\.png$|\.gif$/,
+			loader: 'file-loader?name=/img/[name].[ext]'
+		},{
+            test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+            loader: 'url-loader'
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'css-loader!sass-loader'
+            })
+        }, {
+            test: /bootstrap-sass\/assets\/javascripts\//,
+            loader: 'imports-loader?jQuery=jquery'
+        }]
+    },
+    devServer: {
+        contentBase: './dist',
+        inline: true,
+        open: true,
+        openPage: '',
+        host: '192.168.1.114',
+        port: 8080
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+// require('bootstrap-loader');
+import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'
+import 'bootstrap-sass/assets/javascripts/bootstrap.min.js'
 import './sass/main.scss';
